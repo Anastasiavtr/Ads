518d92768d054f8e4ba65ae2b67690a511a6cd50
Anastasia <100030301+Anastasiavtr@users.noreply.github.com>
2022-05-26 14:29:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-let body = document.body;
 let mainBtn = document.querySelector(".btn");
-let content = document.querySelector(".content");
-let info = document.querySelector(".info");
 let page = document.querySelector(".page");
-let key = document.querySelector(".key__content");
 let keyBtn = document.querySelector(".btn.btn--left");
 let popup = document.querySelector(".pop-up");
 let popupSlider = document.querySelector(".pop-up__slider-list");
-let wrapper = document.querySelector(".key__wrapper");
-let btn = document.querySelector(".close-btn");
-btnLeft = document.querySelector(".btn-left");
-btnRight = document.querySelector(".btn-right");
+let wrapper = document.querySelector(".key__content");
+let closeBtn = document.querySelector(".close-btn");
+let btnLeft = document.querySelector(".btn-left");
+let btnRight = document.querySelector(".btn-right");
+let keyHide = document.querySelector('.key__items')
+let listFirst = document.querySelectorAll(".list-first")
+let listSecond = document.querySelectorAll(".list-second")
 
 
 
 mainBtn.addEventListener("click", moveTo);
 
 function moveTo() {
-  location.href='#info'
+  location.href = '#info'
 
 
   page.classList.remove("page--blue");
@@ -26,61 +25,23 @@ function moveTo() {
 
 
 
-key.addEventListener("click", changeBackground);
-info.addEventListener("click", changeBackground);
-content.addEventListener("click", changeBackground);
-
-function changeBackground(e) {
-  if (e.currentTarget == key) {
-    if (
-      page.classList.contains("page--pink") ||
-      page.classList.contains("page--blue")
-    ) {
-      page.classList.remove("page--pink", "page--blue");
-    }
-    page.classList.add("page--pink-blue");
-  } else if (e.currentTarget == info) {
-    if (
-      page.classList.contains("page--pink-blue") ||
-      page.classList.contains("page--blue")
-    ) {
-      page.classList.remove("page--pink-blue", "page--blue");
-    }
-    page.classList.add("page--pink");
-  } else if (e.currentTarget == content) {
-    if (
-      page.classList.contains("page--pink-blue") ||
-      page.classList.contains("page--pink")
-    ) {
-      page.classList.remove("page--pink-blue", "page--pink");
-    }
-
-    page.classList.add("page--blue");
-  }
-}
-
-
 keyBtn.addEventListener("click", popupShow);
 
 function popupShow() {
-  wrapper.style.cssText = `
-       position: absolute;`;
-
   popup.classList.add("pop-up-active");
- popupSlider.classList.add("pop-up__slider-list-active");
-  page.classList.add("gradient");
+  keyHide.style.visibility = "hidden";
+  popupSlider.classList.add("pop-up__slider-list-active");
+  wrapper.classList.add("gradient");
+
 }
 
 
 
-btn.addEventListener("click", function () {
+closeBtn.addEventListener("click", function () {
   popup.classList.remove("pop-up-active");
+  keyHide.style.visibility = "unset";
   popupSlider.classList.remove("pop-up__slider-list-active");
-
-  wrapper.remove.cssText = `
-        position: absolute;`;
-
-  page.classList.remove("gradient");
+  wrapper.classList.remove("gradient");
 
 });
 
@@ -92,12 +53,28 @@ btnRight.addEventListener("click", () => {
 });
 
 
+
 function changeSlide(direction) {
-  if (direction === "left") {
-    popupSlider.style.transform = `translateY(-25%)`;
+  if (direction === "right") {
+
+    listFirst.forEach(el => el.classList.add("hide"))
+    listSecond.forEach(el => el.classList.remove("hide"))
+
+
     btnLeft.classList.toggle("active-btn");
-  } else if (direction === "right") {
-    popupSlider.style.transform = `translateY(35%)`;
+  } else if (direction === "left") {
+    listFirst.forEach(el => el.classList.remove("hide"))
+    listSecond.forEach(el => el.classList.add("hide"))
+
     btnRight.classList.toggle("active-btn");
   }
+}
+
+
+if (window.innerHeight > window.innerWidth) {
+  portrait = true;
+  landscape = false;
+} else {
+  portrait = false;
+  landscape = true;
 }
